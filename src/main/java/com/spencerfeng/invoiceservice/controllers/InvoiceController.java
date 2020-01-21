dc86321cefc53ca687ea631f6a59d0432c26cd2c
Spencer <spencer@sana.com.au>
2020-01-21 21:21:30 +1100

    Update the create invoice api.

--- src/main/java/com/spencerfeng/invoiceservice/controllers/InvoiceController.java
+++ src/main/java/com/spencerfeng/invoiceservice/controllers/InvoiceController.java
@@ -1,15 +1,11 @@
 package com.spencerfeng.invoiceservice.controllers;
 
-import com.spencerfeng.invoiceservice.dtos.InvoiceCreationDto;
 import com.spencerfeng.invoiceservice.models.Invoice;
-import com.spencerfeng.invoiceservice.repositories.InvoiceRepository;
 import com.spencerfeng.invoiceservice.services.InvoiceService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +15,9 @@ public class InvoiceController {
     @Autowired
     private InvoiceService invoiceService;
 
-    @Autowired
-    private ModelMapper modelMapper;
-
     @PostMapping
-    public ResponseEntity<Invoice> addInvoice(@RequestBody InvoiceCreationDto invoiceDto) {
-        Invoice invoice = modelMapper.map(invoiceDto, Invoice.class);
-
+    public ResponseEntity<Invoice> createInvoice() {
+        Invoice invoice = new Invoice();
         Invoice savedInvoice = invoiceService.addInvoice(invoice);
         return new ResponseEntity<>(savedInvoice, HttpStatus.CREATED);
     }
--- src/test/java/com/spencerfeng/invoiceservice/controllers/InvoiceControllerTests.java
+++ src/test/java/com/spencerfeng/invoiceservice/controllers/InvoiceControllerTests.java
@@ -1,11 +1,8 @@
 package com.spencerfeng.invoiceservice.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.spencerfeng.invoiceservice.dtos.InvoiceCreationDto;
 import com.spencerfeng.invoiceservice.models.Invoice;
-import com.spencerfeng.invoiceservice.models.InvoiceStatus;
 import com.spencerfeng.invoiceservice.services.InvoiceService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,7 +16,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(InvoiceController.class)
@@ -31,42 +27,18 @@ public class InvoiceControllerTests {
     @MockBean
     private InvoiceService invoiceService;
 
-    @MockBean
-    private ModelMapper modelMapper;
-
     @Test
     public void addInvoice_test() throws Exception {
-        InvoiceCreationDto invoiceDto = new InvoiceCreationDto(
-                "Test Business",
-                "Test Client",
-                InvoiceStatus.SENT,
-                "Additional Info"
-        );
-
         Invoice invoice = new Invoice();
-        invoice.setBusiness("Test Business");
-        invoice.setClient("Test Client");
-        invoice.setStatus(InvoiceStatus.SENT);
-        invoice.setAdditionalInfo("Additional Info");
-
-        when(modelMapper.map(invoiceDto, Invoice.class)).thenReturn(invoice);
 
         when(invoiceService.addInvoice(invoice)).thenReturn(invoice);
 
         RequestBuilder request = MockMvcRequestBuilders
                 .post("/api/v1/invoices")
-                .content(asJsonString(invoiceDto))
-                .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON);
 
         mockMvc.perform(request)
-                .andExpect(status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.business").value("Test Business"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.client").value("Test Client"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("SENT"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.additionalInfo").value("Additional Info"));
-
-
+                .andExpect(status().isCreated());
     }
 
     public static String asJsonString(final Object obj) {
